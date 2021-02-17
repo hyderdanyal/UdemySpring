d20d72a65dc5906b5b189d70a21a3f1232a62722
hyderdanyal <hyderdanyal@gmail.com>
2021-02-17 16:01:14 -0600

    changed requestmapping to getmapping or postmapping

--- web customer tracker/src/com/danyalhyder/springdemo/controller/CustomerController.java
+++ web customer tracker/src/com/danyalhyder/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.danyalhyder.springdemo.dao.CustomerDAO;
@@ -20,7 +21,7 @@ public class CustomerController {
 	private CustomerDAO customerDAO;
 	
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomer(Model theModel) {
 		
 		//Get the customers from the dao
