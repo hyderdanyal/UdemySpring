c632217ee6599f50b08b510238d492cb40be015f
hyderdanyal <hyderdanyal@gmail.com>
2021-02-18 21:36:07 -0600

    added @PathVariable for specific students data retrieval

--- spring-rest-demo/src/main/java/com/danyalhyder/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/danyalhyder/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.danyalhyder.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,17 @@ import com.danyalhyder.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+	
+	private List<Student> theStudents;
+	
+	//define @PostConstruct to load student data... only once
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
+		theStudents.add(new Student("Poornima","Patel"));
+		theStudents.add(new Student("Valentino","Rossi"));
+		theStudents.add(new Student("Mary","Lee"));
+	}
 
 	public StudentRestController() {
 		// TODO Auto-generated constructor stub
@@ -21,13 +35,15 @@ public class StudentRestController {
 	@GetMapping("/students")
 	public List<Student> getStudents(){
 		
-		List<Student> theStudents = new ArrayList<>();
-		theStudents.add(new Student("Poornima","Patel"));
-		theStudents.add(new Student("Valentino","Rossi"));
-		theStudents.add(new Student("Mary","Lee"));
-		
-		
 		return theStudents;
+	}
+	
+	//Define endpoint for /students/{studentId} and return particular student
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		//Index the list to the arguement id
+		return theStudents.get(studentId);
 	}
 	
 }
