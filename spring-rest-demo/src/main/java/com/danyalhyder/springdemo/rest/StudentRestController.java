92b02155dff04d0fa140b54a1d38c0ca3f91e3e9
hyderdanyal <hyderdanyal@gmail.com>
2021-02-19 16:19:02 -0600

    Added a global Exception Handler

--- spring-rest-demo/src/main/java/com/danyalhyder/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/danyalhyder/springdemo/rest/StudentRestController.java
@@ -55,35 +55,35 @@ public class StudentRestController {
 		return theStudents.get(studentId);
 	}
 	
-	//Add an exception handler using @ExceptionHandler for studentnotfound
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-		
-		//Create a StudentErrorResponse
-		
-		StudentErrorResponse error = new StudentErrorResponse();
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		//Return ResponseEntity
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-	}
-	
-	//Add anotheer exception handler for all types of exception
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-		
-		//Create a StudentErrorResponse
-		
-				StudentErrorResponse error = new StudentErrorResponse();
-				error.setStatus(HttpStatus.BAD_REQUEST.value());
-				error.setMessage(exc.getMessage());
-				error.setTimeStamp(System.currentTimeMillis());
-				
-				//Return ResponseEntity
-				return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-	
-	}
+//	//Add an exception handler using @ExceptionHandler for studentnotfound
+//	@ExceptionHandler
+//	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+//		
+//		//Create a StudentErrorResponse
+//		
+//		StudentErrorResponse error = new StudentErrorResponse();
+//		error.setStatus(HttpStatus.NOT_FOUND.value());
+//		error.setMessage(exc.getMessage());
+//		error.setTimeStamp(System.currentTimeMillis());
+//		
+//		//Return ResponseEntity
+//		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+//	}
+//	
+//	//Add anotheer exception handler for all types of exception
+//	@ExceptionHandler
+//	public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+//		
+//		//Create a StudentErrorResponse
+//		
+//				StudentErrorResponse error = new StudentErrorResponse();
+//				error.setStatus(HttpStatus.BAD_REQUEST.value());
+//				error.setMessage(exc.getMessage());
+//				error.setTimeStamp(System.currentTimeMillis());
+//				
+//				//Return ResponseEntity
+//				return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+//	
+//	}
 	
 }
