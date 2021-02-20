076b9579fde322bf148191c7c637f873650d407b
hyderdanyal <hyderdanyal@gmail.com>
2021-02-19 18:05:29 -0600

    Added delete customer support in our REST API CRUD app

--- spring-crm-rest-demo/src/main/java/com/danyalhyder/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/danyalhyder/springdemo/rest/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.danyalhyder.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,7 +61,20 @@ public class CustomerRestController {
 		customerService.saveCustomer(theCustomer);
 		return theCustomer; 
 	}
+	
+	//Add mapping for Delete/customers/{customerId} -  delete customer after chefcking it exists
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
 		
+		Customer tempCustomer = customerService.getCustomer(customerId);
+		if (tempCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " +customerId);
+		}
+		
+		customerService.deleteCustomer(customerId);
+		return "Deleted customer id - " +customerId ; 
+	}
+	
 		
 		
 }
